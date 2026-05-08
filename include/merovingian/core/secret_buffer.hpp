2f634482b99ed3045943077a08edace61866f1d1
James Chapman <james@uplinkzero.com>
2026-05-08 23:44:36 +0300

    Add SecretBuffer interface

---
+++ include/merovingian/core/secret_buffer.hpp
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <span>
+#include <vector>
+
+namespace merovingian::core {
+
+class SecretBuffer final {
+public:
+    SecretBuffer() = default;
+
+    explicit SecretBuffer(std::size_t size);
+
+    SecretBuffer(SecretBuffer const&) = delete;
+    auto operator=(SecretBuffer const&) -> SecretBuffer& = delete;
+
+    SecretBuffer(SecretBuffer&&) noexcept = default;
+    auto operator=(SecretBuffer&&) noexcept -> SecretBuffer& = default;
+
+    ~SecretBuffer();
+
+    [[nodiscard]] auto bytes() noexcept -> std::span<std::uint8_t>;
+    [[nodiscard]] auto bytes() const noexcept -> std::span<std::uint8_t const>;
+
+private:
+    std::vector<std::uint8_t> buffer_{};
+};
+
+} // namespace merovingian::core
