7642bd64cc8fbe46cac6192c2396c5c9bc3cb5c8
James Chapman <james@uplinkzero.com>
2026-05-09 00:00:20 +0300

    Convert FileDescriptor tests to Given When Then format

--- tests/unit/test_file_descriptor.cpp
+++ tests/unit/test_file_descriptor.cpp
@@ -7,21 +7,26 @@
 #include <unistd.h>
 
 TEST_CASE("FileDescriptor defaults to invalid", "[core][file_descriptor]") {
+    // Given / When
     auto fd = merovingian::core::FileDescriptor{};
 
+    // Then
     REQUIRE_FALSE(fd.valid());
     REQUIRE(fd.get() == merovingian::core::FileDescriptor::invalid);
 }
 
 TEST_CASE("FileDescriptor can release ownership", "[core][file_descriptor]") {
+    // Given
     int raw_fds[2]{};
     REQUIRE(::pipe(raw_fds) == 0);
 
     auto read_end = merovingian::core::FileDescriptor{raw_fds[0]};
     auto write_end = merovingian::core::FileDescriptor{raw_fds[1]};
 
+    // When
     auto const released = read_end.release();
 
+    // Then
     REQUIRE_FALSE(read_end.valid());
     REQUIRE(released == raw_fds[0]);
 
