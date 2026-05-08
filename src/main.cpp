a1f5712c918d11707de483173b2b053e4d914b24
James Chapman <james@uplinkzero.com>
2026-05-09 00:14:34 +0300

    Add minimal server bootstrap executable

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <merovingian/observability/logger.hpp>
+
+auto main() -> int
+{
+    LOG_INFO("Starting The Merovingian bootstrap server");
+
+    return 0;
+}
