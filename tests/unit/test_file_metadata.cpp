859a7c12165952c6aebc09781789bc30d5d38d8c
James Chapman <james@uplinkzero.com>
2026-05-09 12:33:39 +0300

    Update file metadata tests for platform namespace

--- tests/unit/test_file_metadata.cpp
+++ tests/unit/test_file_metadata.cpp
@@ -1,52 +1,52 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#include <merovingian/core/file_metadata.hpp>
+#include <merovingian/platform/file_metadata.hpp>
 
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("Config file permission policy accepts regular non-executable files", "[core][file-metadata]")
+TEST_CASE("Config file permission policy accepts regular non-executable files", "[platform][file-metadata]")
 {
     // Given
-    auto metadata = merovingian::core::FileMetadata{};
-    metadata.kind = merovingian::core::FileKind::regular;
+    auto metadata = merovingian::platform::FileMetadata{};
+    metadata.kind = merovingian::platform::FileKind::regular;
     metadata.mode.owner_read = true;
     metadata.mode.owner_write = true;
     metadata.mode.group_read = true;
 
     // When / Then
-    REQUIRE(merovingian::core::is_secure_config_file(metadata));
+    REQUIRE(merovingian::platform::is_secure_config_file(metadata));
 }
 
-TEST_CASE("Config file permission policy rejects unsafe file kinds and modes", "[core][file-metadata]")
+TEST_CASE("Config file permission policy rejects unsafe file kinds and modes", "[platform][file-metadata]")
 {
     // Given
-    auto group_writable = merovingian::core::FileMetadata{};
-    group_writable.kind = merovingian::core::FileKind::regular;
+    auto group_writable = merovingian::platform::FileMetadata{};
+    group_writable.kind = merovingian::platform::FileKind::regular;
     group_writable.mode.group_write = true;
 
-    auto other_writable = merovingian::core::FileMetadata{};
-    other_writable.kind = merovingian::core::FileKind::regular;
+    auto other_writable = merovingian::platform::FileMetadata{};
+    other_writable.kind = merovingian::platform::FileKind::regular;
     other_writable.mode.other_write = true;
 
-    auto executable = merovingian::core::FileMetadata{};
-    executable.kind = merovingian::core::FileKind::regular;
+    auto executable = merovingian::platform::FileMetadata{};
+    executable.kind = merovingian::platform::FileKind::regular;
     executable.mode.owner_execute = true;
 
-    auto symlink = merovingian::core::FileMetadata{};
-    symlink.kind = merovingian::core::FileKind::symlink;
+    auto symlink = merovingian::platform::FileMetadata{};
+    symlink.kind = merovingian::platform::FileKind::symlink;
 
     // When / Then
-    REQUIRE_FALSE(merovingian::core::is_secure_config_file(group_writable));
-    REQUIRE_FALSE(merovingian::core::is_secure_config_file(other_writable));
-    REQUIRE_FALSE(merovingian::core::is_secure_config_file(executable));
-    REQUIRE_FALSE(merovingian::core::is_secure_config_file(symlink));
+    REQUIRE_FALSE(merovingian::platform::is_secure_config_file(group_writable));
+    REQUIRE_FALSE(merovingian::platform::is_secure_config_file(other_writable));
+    REQUIRE_FALSE(merovingian::platform::is_secure_config_file(executable));
+    REQUIRE_FALSE(merovingian::platform::is_secure_config_file(symlink));
 }
 
-TEST_CASE("Secret file permission policy requires owner-only non-executable access", "[core][file-metadata]")
+TEST_CASE("Secret file permission policy requires owner-only non-executable access", "[platform][file-metadata]")
 {
     // Given
-    auto secure_secret = merovingian::core::FileMetadata{};
-    secure_secret.kind = merovingian::core::FileKind::regular;
+    auto secure_secret = merovingian::platform::FileMetadata{};
+    secure_secret.kind = merovingian::platform::FileKind::regular;
     secure_secret.mode.owner_read = true;
     secure_secret.mode.owner_write = true;
 
@@ -60,8 +60,8 @@ TEST_CASE("Secret file permission policy requires owner-only non-executable acce
     executable.mode.owner_execute = true;
 
     // When / Then
-    REQUIRE(merovingian::core::is_secure_secret_file(secure_secret));
-    REQUIRE_FALSE(merovingian::core::is_secure_secret_file(group_readable));
-    REQUIRE_FALSE(merovingian::core::is_secure_secret_file(other_readable));
-    REQUIRE_FALSE(merovingian::core::is_secure_secret_file(executable));
+    REQUIRE(merovingian::platform::is_secure_secret_file(secure_secret));
+    REQUIRE_FALSE(merovingian::platform::is_secure_secret_file(group_readable));
+    REQUIRE_FALSE(merovingian::platform::is_secure_secret_file(other_readable));
+    REQUIRE_FALSE(merovingian::platform::is_secure_secret_file(executable));
 }
