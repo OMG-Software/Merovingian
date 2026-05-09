5a6af9f53c5d83e6a1c87126c1a3e119224cb5b7
James Chapman <james@uplinkzero.com>
2026-05-09 21:51:45 +0300

    Use Catch2 BDD macros in runtime media tests

--- tests/unit/test_runtime_media.cpp
+++ tests/unit/test_runtime_media.cpp
@@ -7,46 +7,58 @@
 
 #include <string>
 
-TEST_CASE("Runtime media config parses bounded media limits", "[media][runtime]")
+SCENARIO("Runtime media config parses bounded media limits", "[media][runtime]")
 {
-    // Given
-    auto security = merovingian::config::SecurityConfig{};
-    security.media.max_upload_size = "25MiB";
-    security.media.remote_fetch_timeout = "45s";
-    auto const config = merovingian::config::Config{
-        merovingian::config::ServerConfig{},
-        merovingian::config::ListenersConfig{},
-        merovingian::config::DatabaseConfig{},
-        security,
-    };
+    GIVEN("configuration with bounded media limits")
+    {
+        auto security = merovingian::config::SecurityConfig{};
+        security.media.max_upload_size = "25MiB";
+        security.media.remote_fetch_timeout = "45s";
+        auto const config = merovingian::config::Config{
+            merovingian::config::ServerConfig{},
+            merovingian::config::ListenersConfig{},
+            merovingian::config::DatabaseConfig{},
+            security,
+        };
 
-    // When
-    auto const runtime_media = merovingian::media::make_runtime_media_config(config);
+        WHEN("the runtime media config is created")
+        {
+            auto const runtime_media = merovingian::media::make_runtime_media_config(config);
 
-    // Then
-    REQUIRE(runtime_media.max_upload_bytes == 26214400U);
-    REQUIRE(runtime_media.remote_fetch_timeout_seconds == 45U);
-    REQUIRE(runtime_media.quarantine_unknown_mime);
-    REQUIRE(runtime_media.enable_av_scanner);
-    REQUIRE(runtime_media.private_address_fetches_blocked);
-    REQUIRE(runtime_media.decode_in_sandbox);
+            THEN("bounded operational values are parsed")
+            {
+                REQUIRE(runtime_media.max_upload_bytes == 26214400U);
+                REQUIRE(runtime_media.remote_fetch_timeout_seconds == 45U);
+                REQUIRE(runtime_media.quarantine_unknown_mime);
+                REQUIRE(runtime_media.enable_av_scanner);
+                REQUIRE(runtime_media.private_address_fetches_blocked);
+                REQUIRE(runtime_media.decode_in_sandbox);
+            }
+        }
+    }
 }
 
-TEST_CASE("Runtime media summary exposes bounded operational values", "[media][runtime]")
+SCENARIO("Runtime media summary exposes bounded operational values", "[media][runtime]")
 {
-    // Given
-    auto runtime_media = merovingian::media::RuntimeMediaConfig{};
-    runtime_media.max_upload_bytes = 52428800U;
-    runtime_media.remote_fetch_timeout_seconds = 30U;
-    runtime_media.private_address_fetches_blocked = true;
-    runtime_media.decode_in_sandbox = true;
+    GIVEN("a runtime media config")
+    {
+        auto runtime_media = merovingian::media::RuntimeMediaConfig{};
+        runtime_media.max_upload_bytes = 52428800U;
+        runtime_media.remote_fetch_timeout_seconds = 30U;
+        runtime_media.private_address_fetches_blocked = true;
+        runtime_media.decode_in_sandbox = true;
 
-    // When
-    auto const summary = merovingian::media::media_summary(runtime_media);
+        WHEN("the media summary is generated")
+        {
+            auto const summary = merovingian::media::media_summary(runtime_media);
 
-    // Then
-    REQUIRE(summary.find("max_upload_bytes=52428800") != std::string::npos);
-    REQUIRE(summary.find("remote_fetch_timeout_seconds=30") != std::string::npos);
-    REQUIRE(summary.find("private_address_fetches_blocked=true") != std::string::npos);
-    REQUIRE(summary.find("decode_in_sandbox=true") != std::string::npos);
+            THEN("the expected operational values are present")
+            {
+                REQUIRE(summary.find("max_upload_bytes=52428800") != std::string::npos);
+                REQUIRE(summary.find("remote_fetch_timeout_seconds=30") != std::string::npos);
+                REQUIRE(summary.find("private_address_fetches_blocked=true") != std::string::npos);
+                REQUIRE(summary.find("decode_in_sandbox=true") != std::string::npos);
+            }
+        }
+    }
 }
