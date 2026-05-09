47e10c8ac5746a1a0a0e74e30d17a279c155d5cd
James Chapman <james@uplinkzero.com>
2026-05-09 14:38:01 +0300

    Expand startup hardening self-check shape

--- src/platform/hardening_self_check.cpp
+++ src/platform/hardening_self_check.cpp
@@ -27,6 +27,15 @@ namespace
 #endif
 }
 
+[[nodiscard]] auto compile_time_pie_status() noexcept -> HardeningStatus
+{
+#if defined(__PIE__) || defined(__pie__)
+    return HardeningStatus::enabled;
+#else
+    return HardeningStatus::unknown;
+#endif
+}
+
 } // namespace
 
 HardeningSelfCheck::HardeningSelfCheck(std::vector<HardeningCheck> checks)
@@ -47,11 +56,20 @@ auto HardeningSelfCheck::count() const noexcept -> std::size_t
 auto run_startup_hardening_self_check() -> HardeningSelfCheck
 {
     auto checks = std::vector<HardeningCheck>{};
-    checks.reserve(4U);
+    checks.reserve(13U);
+    checks.push_back({"compiler hardening", HardeningStatus::unknown});
+    checks.push_back({"linker hardening", HardeningStatus::unknown});
+    checks.push_back({"PIE", compile_time_pie_status()});
+    checks.push_back({"RELRO", HardeningStatus::unknown});
     checks.push_back({"stack protector", compile_time_stack_protector_status()});
     checks.push_back({"FORTIFY_SOURCE", compile_time_fortify_status()});
-    checks.push_back({"runtime sandbox", HardeningStatus::unknown});
+    checks.push_back({"seccomp", HardeningStatus::unknown});
+    checks.push_back({"pledge/unveil", HardeningStatus::unknown});
+    checks.push_back({"capsicum", HardeningStatus::unknown});
+    checks.push_back({"privilege drop", HardeningStatus::unknown});
     checks.push_back({"filesystem restrictions", HardeningStatus::unknown});
+    checks.push_back({"core dump policy", HardeningStatus::unknown});
+    checks.push_back({"secret redaction policy", HardeningStatus::enabled});
 
     return HardeningSelfCheck{std::move(checks)};
 }
