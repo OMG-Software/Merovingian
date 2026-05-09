06b226c9e850c663d0153fc77bc7b90b0131e322
James Chapman <james@uplinkzero.com>
2026-05-09 18:38:37 +0300

    Use Given When Then in HTTP connection guard tests

--- tests/unit/test_http_connection_guard.cpp
+++ tests/unit/test_http_connection_guard.cpp
@@ -8,9 +8,14 @@
 
 TEST_CASE("HTTP slowloris policy validates conservative bounds", "[http][slowloris]")
 {
+    // Given
     auto const policy = merovingian::http::SlowlorisPolicy{};
 
-    REQUIRE(merovingian::http::slowloris_policy_is_valid(policy));
+    // When
+    auto const valid = merovingian::http::slowloris_policy_is_valid(policy);
+
+    // Then
+    REQUIRE(valid);
     REQUIRE(policy.min_bytes_per_second == 64U);
     REQUIRE(policy.grace_seconds == 5U);
     REQUIRE(policy.header_deadline_seconds == 30U);
@@ -18,18 +23,31 @@ TEST_CASE("HTTP slowloris policy validates conservative bounds", "[http][slowlor
 
 TEST_CASE("HTTP slowloris guard allows grace period and rejects slow progress", "[http][slowloris]")
 {
+    // Given
     auto const policy = merovingian::http::SlowlorisPolicy{};
 
-    REQUIRE_FALSE(merovingian::http::request_progress_is_too_slow({0U, 5U}, policy));
-    REQUIRE(merovingian::http::request_progress_is_too_slow({63U, 6U}, policy));
-    REQUIRE_FALSE(merovingian::http::request_progress_is_too_slow({64U, 6U}, policy));
-    REQUIRE(merovingian::http::request_progress_is_too_slow({4096U, 31U}, policy));
+    // When
+    auto const grace_period_too_slow = merovingian::http::request_progress_is_too_slow({0U, 5U}, policy);
+    auto const under_rate_too_slow = merovingian::http::request_progress_is_too_slow({63U, 6U}, policy);
+    auto const at_rate_too_slow = merovingian::http::request_progress_is_too_slow({64U, 6U}, policy);
+    auto const deadline_exceeded_too_slow = merovingian::http::request_progress_is_too_slow({4096U, 31U}, policy);
+
+    // Then
+    REQUIRE_FALSE(grace_period_too_slow);
+    REQUIRE(under_rate_too_slow);
+    REQUIRE_FALSE(at_rate_too_slow);
+    REQUIRE(deadline_exceeded_too_slow);
 }
 
 TEST_CASE("HTTP slowloris policy summary is stable", "[http][slowloris]")
 {
-    auto const summary = merovingian::http::slowloris_policy_summary(merovingian::http::SlowlorisPolicy{});
+    // Given
+    auto const policy = merovingian::http::SlowlorisPolicy{};
 
+    // When
+    auto const summary = merovingian::http::slowloris_policy_summary(policy);
+
+    // Then
     REQUIRE(summary.find("min_bytes_per_second=64") != std::string::npos);
     REQUIRE(summary.find("grace_seconds=5") != std::string::npos);
     REQUIRE(summary.find("header_deadline_seconds=30") != std::string::npos);
