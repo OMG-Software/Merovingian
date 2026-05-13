37714da0c34874c92dce30153455813c7cc9ffdb
James Chapman <github@ping.me.uk>
2026-05-13 11:22:31 +0100

    More include fixes

--- tests/unit/test_database_persistence.cpp
+++ tests/unit/test_database_persistence.cpp
@@ -2,13 +2,19 @@
 
 #include "merovingian/database/connection.hpp"
 #include "merovingian/database/migration.hpp"
+#include "merovingian/database/migration_files.hpp"
 #include "merovingian/database/persistent_store.hpp"
+#include "merovingian/database/postgresql_store.hpp"
 #include "merovingian/database/schema.hpp"
 #include "merovingian/database/statement.hpp"
 
 #include <catch2/catch_test_macros.hpp>
 
+#include <chrono>
+#include <filesystem>
+#include <fstream>
 #include <string>
+#include <type_traits>
 #include <vector>
 
 namespace
